d8e2086479fcda9708d2c010e018221e449de879
Dekillston <75752521+Dekillston@users.noreply.github.com>
2023-08-31 18:20:04 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const { google } = require("googleapis");
+//
+function NodeGoogleSheets(file, sheetId, keyMass, fun) {
+	const auth = new google.auth.GoogleAuth({
+		keyFile: file,
+		scopes: "https://www.googleapis.com/auth/spreadsheets",
+	});
+	//
+	(async () => {
+		const client = await auth.getClient();
+		//
+		const googleSheets = google.sheets({ version: "v4", auth: client });
+		//
+		const spreadsheetId = sheetId;
+		//
+		const metaData = await googleSheets.spreadsheets.get({
+			auth,
+			spreadsheetId,
+		});
+		//
+		const data = {
+			auth,
+			spreadsheetId,
+			valueInputOption: "USER_ENTERED",
+			resource: {
+				values: keyMass.change,
+			},
+		}
+		//
+		if(keyMass.append) {
+			data['range'] = keyMass.append;
+			//
+			const append = await googleSheets.spreadsheets.values.append(data);
+			//
+			fun(append);
+		} else if(keyMass.values) {
+			data['range'] = keyMass.values;
+			//
+			delete data.valueInputOption; delete data.resource;
+			//
+			const values = await googleSheets.spreadsheets.values.get(data);
+			//
+			fun(values); 
+		} else if(keyMass.update) {
+			data['range'] = keyMass.update;
+			//
+			const update = await googleSheets.spreadsheets.values.update(data);
+			//
+			fun(update);
+		}
+	})();
+}
